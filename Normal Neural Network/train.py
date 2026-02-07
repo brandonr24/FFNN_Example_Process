903c96b369171e2c9008af8c6d1b4d83584a3354
Brandon R <brandonr@vt.edu>
2026-02-07 14:32:45 -0500

    Overwrote work for this semester

---
+++ Normal Neural Network/train.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+h_ranges = [0.01,0.005,0.001,0.0005,0.0001]
+d, w0 = 2, 20
+s0 = [1, 0] # Initial Condition
+
+def plot_exact_solution(d, w0):
+    x = torch.linspace(0,1,500).view(-1,1)
+
+    assert d < w0
+    w = np.sqrt(w0**2-d**2)
+    phi = np.arctan(-d/w)
+    A = 1/(2*np.cos(phi))
+    cos = torch.cos(phi+w*x)
+    sin = torch.sin(phi+w*x)
+    exp = torch.exp(-d*x)
+    y  = exp*2*A*cos
+    
+    plt.plot(x, y, 'black', label='Exact')
+
+def f(t_i, s_i):
+    return [s_i[1], (-2*d*s_i[1]-w0**2*s_i[0])]
+
+def euler_method(h):
+    t = np.arange(0, 1 + h, h)
+
+    # Explicit Euler Method
+    s = np.zeros((len(t), 2))
+    s[0] = s0
+
+    for i in range(0, len(t) - 1):
+        s[i + 1] = [s[i][j] + h*f(t[i], s[i])[j] for j in range(2)]
+        
+    plt.plot(t, [s[i][0] for i in range(len(s))] , '--', label=f'Approximate With {h} Step Size')
+
+plt.figure(figsize = (12, 8))
+for k in h_ranges:
+    euler_method(k)
+plot_exact_solution(d, 20)
+plt.ylim(-0.75, 1.05)
+plt.xlabel('t')
+plt.ylabel('f(t)')
+plt.grid()
+plt.legend(loc='lower right')
+file = "finite_differences_step_sizes"
+# plt.savefig(file, bbox_inches='tight', pad_inches=0.1, dpi=100, facecolor="white")
+plt.show()
